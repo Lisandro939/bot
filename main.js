dbc1af96a323a53d6cc5eec96e39b751df2b0151
Lisandro939 <lisandrofernandez2705@gmail.com>
2025-07-09 18:35:50 -0300

    fix: pupeteer error

--- main.js
+++ main.js
@@ -204,8 +204,13 @@ app.get('/qr-image', (req, res) => {
 });
 
 const client = new Client({
-    authStrategy: new LocalAuth()
-});
+authStrategy: new LocalAuth({
+    dataPath: "sessions",
+}),
+webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+}});
 
 let isClientReady = false;
 let currentQR = null;
